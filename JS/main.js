b91633fcaeba1bbd317c56563189c302f9a3c6d2
Jose M. Vasconcellos <miguel_vasconce@hotmail.com>
2018-08-03 17:08:22 -0400

    Added last 2 html pages

--- JS/main.js
+++ JS/main.js
@@ -4,40 +4,35 @@ var window_width = 0,
     mid_container_width = 0;
 
 function setOffsets(){
-  window_width = $(window).width();
-  mid_container_offset = $(".mid-container").offset();
-  mid_container_width = $(".mid-container").width();
-  console.log(window_width);  //690px
+    window_width = $(window).width();
+    mid_container_offset = $(".mid-container").offset();
+    mid_container_width = $(".mid-container").width();
+    console.log(window_width);  //690px
+}
+
+function moveContainers(){
+    if(window_width >= 675){
+      $(".logo").css("margin-left",mid_container_offset.left-8);
+      $(".nav-links").css("margin-right",((window_width - mid_container_width)/2)-38);
+    }else{
+      $(".logo").css("margin-left",0);
+      $(".nav-links").css("margin-right",0);
+    }
 }
 
 function toggleVisible(){
-  $(".logo").css("visibility","visible");
-  $(".nav-links").css("visibility","visible");
+    $(".logo").css("visibility","visible");
+    $(".nav-links").css("visibility","visible");
 }
 
 $(document).ready(function(){
-  setOffsets();
-  if(window_width >= 675){
-    $(".logo").css("margin-left",mid_container_offset.left-8);
-    $(".nav-links").css("margin-right",((window_width - mid_container_width)/2)-38);
-  }else{
-    $(".logo").css("margin-left",0);
-    $(".nav-links").css("margin-right",0);
-  }
-  toggleVisible();
+    setOffsets();
+    moveContainers();
+    toggleVisible();
 });
 
 $(window).resize(function(){
-  setOffsets();
-
-  if(window_width >= 675){
-    $(".logo").css("margin-left",mid_container_offset.left-8);
-    $(".nav-links").css("margin-right",((window_width - mid_container_width)/2)-38);
-  }else{
-    $(".logo").css("margin-left",0);
-    $(".nav-links").css("margin-right",0);
-  }
-
-
+    setOffsets();
+    moveContainers();
 });
 /***********************/
